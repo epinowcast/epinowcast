df0b8a9c9fbf05692e55dd232db9bf4ee175cc2c
Sam Abbott <contact@samabbott.co.uk>
2022-03-24 18:21:12 +0000

    Add stanc_options and enable optimisations

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
-# epinowcast 0.0.4.9000
+# epinowcast 0.0.4.10000
 
 * Convert retrospective data date fields to class of `IDate` when utilising `enw_retrospective_data` to solve esoteric error.
 * Added full argument name for `include_paths` to avoid console chatter
+* Adds a `stanc_options` argument to `enw_model()` and specifies a new default of `list("01")` which enables simple pre-compilation optimisations. See [here](https://blog.mc-stan.org/2022/02/15/release-of-cmdstan-2-29/) of these optimisatiosn for details.
 
 # epinowcast 0.0.4
 
--- R/model.R
+++ R/model.R
@@ -217,6 +217,7 @@ enw_inits <- function(data) {
 #'
 #' @param include A character string specifying the path to any stan
 #' files to include in the model. If missing the package default is used.
+#'
 #' @param compile Logical, defaults to `TRUE`. Should the model
 #' be loaded and compiled using [cmdstanr::cmdstan_model()].
 #'
@@ -227,6 +228,10 @@ enw_inits <- function(data) {
 #'
 #' @param verbose Logical, defaults to `TRUE`. Should verbose
 #' messages be shown.
+#'
+#' @param stanc_options A list of options to pass to the `stanc_options` of [cmdstanr::cmdstan_model()]
+#' by default "01" is passed which specifies simple optimisations should be done by the prior to compilation
+#'
 #' @param ... Additional arguments passed to [cmdstanr::cmdstan_model()].
 #'
 #' @return A `cmdstanr` model.
@@ -237,7 +242,7 @@ enw_inits <- function(data) {
 #' @examplesIf interactive()
 #' mod <- enw_model()
 enw_model <- function(model, include,
-                      compile = TRUE, threads = FALSE, verbose = TRUE, ...) {
+                      compile = TRUE, threads = FALSE, stanc_options = list("O1"), verbose = TRUE, ...) {
   if (missing(model)) {
     model <- "stan/epinowcast.stan"
     model <- system.file(model, package = "epinowcast")
@@ -250,6 +255,7 @@ enw_model <- function(model, include,
     if (verbose) {
       model <- cmdstanr::cmdstan_model(model,
         include_paths = include,
+        stanc_options = stanc_options,
         cpp_options = list(
           stan_threads = threads
         ),
@@ -259,6 +265,7 @@ enw_model <- function(model, include,
       suppressMessages(
         model <- cmdstanr::cmdstan_model(model,
           include_paths = include,
+          stanc_options = stanc_options,
           cpp_options = list(
             stan_threads = threads
           ), ...
