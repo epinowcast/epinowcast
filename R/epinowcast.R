29587db68944932677a5a060d72f31dc514834b1
Adrian Lison <alison@ethz.ch>
2022-04-14 12:36:53 +0200

    Revert "Refactor functions/pmfs"

--- R/epinowcast.R
+++ R/epinowcast.R
@@ -27,7 +27,7 @@ epinowcast <- function(pobs,
                          pobs$metareport[[1]]
                        ),
                        priors = epinowcast::enw_priors(),
-                       distribution = "lognormal",
+                       dist = "lognormal",
                        model = epinowcast::enw_model(),
                        as_data_list = epinowcast::enw_as_data_list,
                        inits = epinowcast::enw_inits,
@@ -39,7 +39,7 @@ epinowcast <- function(pobs,
     reference_effects = reference_effects,
     report_effects = report_effects,
     priors = priors,
-    distribution = distribution, nowcast = nowcast,
+    dist = dist, nowcast = nowcast,
     likelihood = likelihood, debug = debug, pp = pp,
     output_loglik = output_loglik
   )
