0dff16ff0539e23d294f2c809e8085c03774af6c
Sam Abbott <contact@samabbott.co.uk>
2022-06-14 16:17:15 +0000

    first pass at fixing scoring

--- R/model-validation.R
+++ R/model-validation.R
@@ -1,18 +1,33 @@
-#' FUNCTION_TITLE
+#' Evaluate nowcasts using proper scoring rules
 #'
-#' FUNCTION_DESCRIPTION
+#' Acts as a wrapper to [scoringutils::score()]. In particular,
+#' handling filtering nowcast summary output and linking this output to
+#' observed data. See the documentation for the `scoringutils` package for more
+#' on forecast scoring.
 #'
-#' @param nowcast DESCRIPTION.
+#' @param nowcast A posterior nnowcast or posterior prediction as returned by
+#' [summary.fv_posterior()], [summary.fv_forecast()] or [fv_extract_forecast()].
+#' Internally case forecasts are filtered for using the `value_type` variable
+#' if present as are only overall or combined case counts (i.e as returned)
+#' by the 1 and 2 strain models. If looking for more complex scoring it may be
+#' wise to implement a custom wrapper.
 #'
-#' @param latest_obs DESCRIPTION.
+#' @param log Logical, defaults to FALSE. Should scores be calculated on the
+#' log scale (with a 0.01 shift) for both observations and nowcasts. Scoring in
+#' this way can be thought of as a relative score vs the more usual absolute
+#' measure. It may be useful when targets are on very different scales or when
+#' the forecaster is more interested in good all round performance versus good
+#' performance for targets with large values.
 #'
-#' @param log DESCRIPTION.
+#' @param check Logical, defaults to FALSE. Should
+#' [scoringutils::check_forecasts()] be used to check input nowcasts.
 #'
-#' @param round_to DESCRIPTION
+#' @param round_to Integer defaults to 3. Number of digits to round scoring
+#' output to.
 #'
-#' @param ... Additional arguments passed to [scoringutils::eval_forecasts()].
+#' @param ... Additional arguments passed to [scoringutils::score()].
 #'
-#' @return RETURN_DESCRIPTION
+#' @return A `data.table` as returned by [scoringutils::score()].
 #' @family modelvalidation
 #' @importFrom data.table copy setnames
 #' @export
@@ -32,7 +47,11 @@ enw_score_nowcast <- function(nowcast, latest_obs, log = FALSE,
     long_nowcast[, (cols) := purrr::map(.SD, ~ log(. + 0.01)), .SDcols = cols]
   }
 
-  scores <- scoringutils::eval_forecasts(long_nowcast, ...)
+  if (check) {
+    scoringutils::check_forecasts(long_nowcast)
+  }
+
+  scores <- scoringutils::score(long_nowcast, ...)
   numeric_cols <- colnames(scores)[sapply(scores, is.numeric)]
   scores <- scores[, (numeric_cols) := lapply(.SD, signif, digits = round_to),
     .SDcols = numeric_cols
