1a07a18f97b454523f0ac8428b9b73d122308e5c
Sam Abbott <contact@samabbott.co.uk>
2022-06-21 10:59:03 +0000

    tune test fit

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,6 @@ if (not_on_cran()) {
   message("Running tests setup")
   options(mc.cores = 2)
   utils::capture.output(
-    source("inst/scripts/germany_example.R")
+    source("../../inst/scripts/germany_example.R")
   )
 }
