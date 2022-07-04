e741f83d43d9447104435389f1cc67477ce6a962
seabbs <s.e.abbott12@gmail.com>
2022-07-04 17:44:04 +0000

    inverse tests

--- tests/testthat/test-stan_fns_as_string.R
+++ tests/testthat/test-stan_fns_as_string.R
@@ -1,7 +1,8 @@
 test_that("stan_fns_as_string can read in a stan function file as expected", {
   expect_error(
     epinowcast:::stan_fns_as_string(
-      "hazard.stan", system.file("stan/functions", packages = "epinowcast")
+      "hazard.stan", system.file("stan/functions", package = "epinowcast"),
+    NA
     )
   )
 })
