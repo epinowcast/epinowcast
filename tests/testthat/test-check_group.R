e5f10264c02dcee140bcf227a45c8068cedd26c2
Sam Abbott <contact@samabbott.co.uk>
2022-07-06 14:52:11 +0000

    add tests for check_group

---
+++ tests/testthat/test-check_group.R
@@ -0,0 +1,11 @@
+test_that("check_group works for example data", {
+  expect_error(
+    epinowcast:::check_group(germany_covid19_hosp), NA
+  )
+})
+
+test_that("check_group fails when reserved variables are present", {
+  expect_error(check_group(data.frame(.group = 1)))
+  expect_error(check_group(data.frame(.old_group = 1)))
+  expect_error(check_group(data.frame(.new_group = 1)))
+})
