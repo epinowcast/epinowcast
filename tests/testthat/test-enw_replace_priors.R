b79ddbd77274873835f68197de69775586ef1760
Sam Abbott <contact@samabbott.co.uk>
2022-07-15 14:43:40 +0000

    add a simple test for enw_replace_priors

---
+++ tests/testthat/test-enw_replace_priors.R
@@ -0,0 +1,9 @@
+test_that("enw_replace_priors can replace a default prior with a custom
+           prior", {
+  priors <- data.frame(variable = c("x", "y"), mean = c(1, 2), sd = c(1, 2))
+  custom_priors <- data.frame(variable = "x", mean = 10, sd = 2)
+  exp_priors <- data.table::data.table(
+    variable = c("y", "x"), mean = c(2, 10), sd = c(2, 2)
+  )
+  expect_equal(enw_replace_priors(priors, custom_priors), exp_priors)
+})
