395dfd7a734af5e267ecd8016aa45c20a2624eed
Sam Abbott <contact@samabbott.co.uk>
2022-07-18 13:59:26 +0000

    fix enw_fit_opts tests

--- tests/testthat/test-enw_fit_opts.R
+++ tests/testthat/test-enw_fit_opts.R
@@ -1,6 +1,4 @@
 test_that("enw_fit_opts produces the expected output", {
   expect_snapshot(enw_fit_opts(sampler = NULL, adapt_delta = 0.9))
-  expect_true(
-    enw_fit_opts(pp = TRUE, nowcast = FALSE)$data$cast
-  )
+  expect_equal(enw_fit_opts(pp = TRUE, nowcast = FALSE)$data$cast, 1)
 })
