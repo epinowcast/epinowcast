8f4a2de04d189fa12bf17cb92fe13a60c6f5b1af
Sam Abbott <contact@samabbott.co.uk>
2022-07-18 18:52:47 +0000

    style pkg

--- tests/testthat/test-enw_formula_as_data_list.R
+++ tests/testthat/test-enw_formula_as_data_list.R
@@ -1,13 +1,14 @@
 test_that("enw_formula_as_data_list fails when expected", {
   expect_error(enw_formula_as_data_list(~ 1 + 1))
-  expect_error(enw_formula_as_data_list(enw_formula(~ 1, mtcars)))
+  expect_error(enw_formula_as_data_list(enw_formula(~1, mtcars)))
 })
 
 test_that("enw_formula_as_data_list produces expected output using a simple
            formula", {
   expect_snapshot(
     enw_formula_as_data_list(
-      enw_formula(~ 1 + (1 | cyl), mtcars), prefix = "simple"
+      enw_formula(~ 1 + (1 | cyl), mtcars),
+      prefix = "simple"
     )
   )
   expect_snapshot(
@@ -22,15 +23,16 @@ test_that("enw_formula_as_data_list produces expected output using a more
            complex formula", {
   expect_snapshot(
     enw_formula_as_data_list(
-      enw_formula(~ 1 + disp +  (1 + gear | cyl) + (0 + wt | am), mtcars),
+      enw_formula(~ 1 + disp + (1 + gear | cyl) + (0 + wt | am), mtcars),
       prefix = "complex"
     )
   )
   expect_snapshot(
     enw_formula_as_data_list(
       enw_formula(
-        ~ 1 + disp +  (1 + gear | cyl) + (0 + wt | am), mtcars),
-        prefix = "comple", drop_intercept = TRUE
+        ~ 1 + disp + (1 + gear | cyl) + (0 + wt | am), mtcars
+      ),
+      prefix = "comple", drop_intercept = TRUE
     )
   )
 })
