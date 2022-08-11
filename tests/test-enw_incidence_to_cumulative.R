dc76f1d6a24c7f71b5f4f51734868213466820b8
Sam Abbott <contact@samabbott.co.uk>
2022-08-11 16:20:59 +0000

    add internal helper functions for missing reference lookup

--- tests/test-enw_incidence_to_cumulative.R
+++
@@ -1,7 +0,0 @@
-test_that("enw_incidence_to_cumulative can return toy cumulative data", {
-  dt <- enw_incidence_to_cumulative(toy_incidence)
-  expect_equal(
-    dt[, .(reference_date, report_date, confirm)],
-    toy_cumulative[order(reference_date, report_date)]
-  )
-})
