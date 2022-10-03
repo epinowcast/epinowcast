9d830c110ef469a12e14b8c9d6b2186a43b30828
Sam Abbott <contact@samabbott.co.uk>
2022-10-03 17:38:40 +0000

    fix priors as data list test

--- tests/testthat/test-enw_priors_as_data_list.R
+++ tests/testthat/test-enw_priors_as_data_list.R
@@ -3,10 +3,14 @@ test_that("enw_priors_as_data_list produces expected output", {
     variable = c("x", "y", "z", "z"), mean = c(1, 2, 2, 3), sd = c(2, 2.2, 3, 1)
   )
   priors_list <- list(
-    x_p = array(t(c(1, 2))),
-    y_p = array(t(c(2, 2.2))),
+    x_p = as.matrix(c(1, 2)),
+    y_p = as.matrix(c(2, 2.2)),
     z_p = as.array(matrix(c(2, 3, 3, 1), 2, 2))
   )
+  rownames(priors_list$x_p) <- c("mean", "sd")
+  rownames(priors_list$y_p) <- c("mean", "sd")
+  rownames(priors_list$z_p) <- c("mean", "sd")
+  
   expect_equal(
     enw_priors_as_data_list(priors), priors_list
   )
