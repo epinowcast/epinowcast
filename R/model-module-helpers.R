d47900cec0acc64b551201d80c8fe69c7220de33
Sam Abbott <contact@samabbott.co.uk>
2022-12-05 15:11:48 +0000

    Bug: Hotfix missing family for latest_obs_as_matrix docs

--- R/model-module-helpers.R
+++ R/model-module-helpers.R
@@ -73,6 +73,7 @@ enw_reference_by_report <- function(missing_reference, reps_with_complete_refs,
 #' @param latest `latest` data.frame output from [enw_preprocess_data()].
 #'
 #' @return A matrix with each column being a group and each row a reference date
+#' @family modelmodulehelpers
 latest_obs_as_matrix <- function(latest) {
   latest_matrix <- data.table::dcast(
     latest, reference_date ~ .group,
