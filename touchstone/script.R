166c17e8e4066442c286f242a0859e49f469468e
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-03-23 14:08:36 +0100

    Test workaround for touchstone

--- touchstone/script.R
+++ touchstone/script.R
@@ -2,7 +2,7 @@
 # interactively
 
 # benchmarks.
-touchstone::pin_assets("touchstone")#, "inst/examples")
+#touchstone::pin_assets("touchstone", "inst/examples")
 
 # installs branches to benchmark
 touchstone::branch_install()
@@ -10,14 +10,14 @@ touchstone::branch_install()
 # run benchmarks
 touchstone::benchmark_run(
   simple = {
-    source(path_pinned_asset("inst/examples/germany_simple.R"))
+    source("inst/examples/germany_simple.R")#source(path_pinned_asset("inst/examples/germany_simple.R"))
   },
   n = 3
 )
 
 touchstone::benchmark_run(
   day_of_week = {
-    source(path_pinned_asset("inst/examples/germany_dow.R"))
+    source("inst/examples/germany_dow.R")#source(path_pinned_asset("inst/examples/germany_dow.R"))
   },
   n = 3
 )
@@ -25,14 +25,14 @@ touchstone::benchmark_run(
 
 touchstone::benchmark_run(
   missingness = {
-    source(path_pinned_asset("inst/examples/germany_missing.R"))
+    source("inst/examples/germany_missing.R")#source(path_pinned_asset("inst/examples/germany_missing.R"))
   },
   n = 3
 )
 
 touchstone::benchmark_run(
   latent_renewal = {
-    source(path_pinned_asset("inst/examples/germany_latent_renewal.R"))
+    source("inst/examples/germany_latent_renewal.R")#source(path_pinned_asset("inst/examples/germany_latent_renewal.R"))
   },
   n = 3
 )
