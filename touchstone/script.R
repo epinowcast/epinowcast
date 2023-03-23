a10c2dd5c8ac30d819897f86df4a2bad032a21b7
Sam <s.e.abbott12@gmail.com>
2023-03-23 19:09:56 +0000

    drop pin_assests again

--- touchstone/script.R
+++ touchstone/script.R
@@ -9,31 +9,23 @@ touchstone::pin_assets("inst/examples")
 
 # run benchmarks
 touchstone::benchmark_run(
-  simple = {
-    source(path_pinned_asset("inst/examples/germany_simple.R"))
-  },
+  simple = { source("inst/examples/germany_simple.R") },
   n = 3
 )
 
 touchstone::benchmark_run(
-  day_of_week = {
-    source(path_pinned_asset("inst/examples/germany_dow.R"))
-  },
+  day_of_week = { source("inst/examples/germany_dow.R") },
   n = 3
 )
 
 
 touchstone::benchmark_run(
-  missingness = {
-    source(path_pinned_asset("inst/examples/germany_missing.R"))
-  },
+  missingness = { source("inst/examples/germany_missing.R") },
   n = 3
 )
 
 touchstone::benchmark_run(
-  latent_renewal = {
-    source(path_pinned_asset("inst/examples/germany_latent_renewal.R"))
-  },
+  latent_renewal = { source("inst/examples/germany_latent_renewal.R") },
   n = 3
 )
 
