26597c423a1c8c184d30760b12eb5bc4b1ef1a88
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-03-23 11:55:16 +0100

    Test pin_assets touchstone as point of failure

--- touchstone/script.R
+++ touchstone/script.R
@@ -2,7 +2,7 @@
 # interactively
 
 # benchmarks.
-touchstone::pin_assets("touchstone")#, "inst/examples")
+touchstone::pin_assets("inst/examples")#, "touchstone)
 
 # installs branches to benchmark
 touchstone::branch_install()
