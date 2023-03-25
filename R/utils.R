db985cdff3faec460585682a0bf55d2bf76b8e5c
Sam <s.e.abbott12@gmail.com>
2023-03-25 22:26:46 +0000

    fix variable notes

--- R/utils.R
+++ R/utils.R
@@ -214,6 +214,6 @@ utils::globalVariables(
     "fit", "patterns", ".draws", "prop_reported", "max_confirm",
     "run_time", "cum_prop_reported", "..by_with_group_id",
     "reference_missing", "prop_missing", "day", "posteriors",
-    "formula", ".id", "n", ".confirm_avail"
+    "formula", ".id", "n", ".confirm_avail", "prediction", "true_value"
   )
 )
