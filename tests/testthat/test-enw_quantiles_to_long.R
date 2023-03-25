bf586d3541eaa23379b9e7d8935c847036986cc2
Sam <s.e.abbott12@gmail.com>
2023-03-25 23:49:10 +0000

    rounding fixed for quantiles_to_long tests

--- tests/testthat/test-enw_quantiles_to_long.R
+++ tests/testthat/test-enw_quantiles_to_long.R
@@ -2,8 +2,9 @@ test_that("enw_quantiles_to_long can manipulate posterior draws as expected", {
   fit <- enw_example("nowcast")
   posterior <- enw_posterior(fit$fit[[1]], var = "expr_lelatent_int[1,1]")
   expect_snapshot(
-    enw_quantiles_to_long(posterior)[,
-     c("rhat", "ess_bulk", "ess_tail") := NULL
-    ][!is.na(variable)][]
+    round_numeric(
+      enw_quantiles_to_long(posterior)[,
+        c("rhat", "ess_bulk", "ess_tail") := NULL][!is.na(variable)][]
+    )
   )
 })
