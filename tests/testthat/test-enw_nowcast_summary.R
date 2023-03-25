ffe1c8137981defe49e9c9099f3e293af6ca7864
Sam <s.e.abbott12@gmail.com>
2023-03-25 14:43:49 +0000

    add snapshot tests for enw_nowcast_summary

---
+++ tests/testthat/test-enw_nowcast_summary.R
@@ -0,0 +1,17 @@
+test_that("enw_nowcast_summary can extract the summarised nowcast as expected", {
+  fit <- enw_example("nowcast")
+  nowcast <- enw_nowcast_summary(fit$fit[[1]], fit$latest[[1]])
+  expect_snapshot(
+    nowcast[, c("rhat", "ess_bulk", "ess_tail") := NULL],
+  )
+})
+
+test_that("enw_nowcast_summary can extract the summarised nowcast with custom quantiles", {
+  fit <- enw_example("nowcast")
+  nowcast <- enw_nowcast_summary(
+    fit$fit[[1]], fit$latest[[1]], probs = c(0.05, 0.5, 0.95)
+  )
+  expect_snapshot(
+    nowcast[, c("rhat", "ess_bulk", "ess_tail") := NULL],
+  )
+})
