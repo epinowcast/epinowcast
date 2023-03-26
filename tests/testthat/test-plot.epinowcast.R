2dc3b7b35436eca98a9cf8cf93990886d289726b
Sam <s.e.abbott12@gmail.com>
2023-03-26 14:52:00 +0100

    drop log scale tests in wrapper

--- tests/testthat/test-plot.epinowcast.R
+++ tests/testthat/test-plot.epinowcast.R
@@ -7,21 +7,8 @@ test_that("plot.epinowcast passes to lower level functions as expected", {
   )
   suppressWarnings(suppressMessages(
     vdiffr::expect_doppelganger(
-      "plot.epinowcast default nowcast with log scale",
-        plot(fit, type = "nowcast", log = TRUE)
-    )
-  ))
-  suppressWarnings(suppressMessages(
-    vdiffr::expect_doppelganger(
       "plot.epinowcast posterior predictions",
       plot(fit, type = "posterior_prediction") +
-        ggplot2::facet_wrap(ggplot2::vars(reference_date), scales = "free")
-    )
-  ))
-  suppressWarnings(suppressMessages(
-    vdiffr::expect_doppelganger(
-      "plot.epinowcast posterior predictions with log scale",
-        plot(fit, type = "posterior_prediction", log = TRUE) +
         ggplot2::facet_wrap(ggplot2::vars(reference_date), scales = "free")
     )
   ))
