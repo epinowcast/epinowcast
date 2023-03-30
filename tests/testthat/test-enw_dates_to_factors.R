93d3575af37c4029aa380a6c05031bebd3c0d67c
Sam <s.e.abbott12@gmail.com>
2023-03-30 13:47:12 +0100

    Hotfix: Remove enw_dates_to_factors test

--- tests/testthat/test-enw_dates_to_factors.R
+++
@@ -1,9 +0,0 @@
-test_that("enw_dates_to_factors can convert date variables", {
-  data <- data.frame(
-    date = as.Date("2019-01-01") + 0:2,
-    char = c("a", "b", "c")
-  )
-  data <- enw_dates_to_factors(data)
-  expect_true(is.factor(data$date))
-  expect_equal(ncol(data), 1)
-})
