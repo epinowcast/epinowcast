e9a42050cc4e885f495f090829850a2d94f5742d
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-04-05 11:55:57 +0200

    Outsource max_delay checks

--- R/check.R
+++ R/check.R
@@ -110,15 +110,54 @@ check_modules_compatible <- function(modules) {
     modules[[4]]$data$model_miss &&
       !modules[[6]]$data$likelihood_aggregation
   ) {
-    warning(paste0(
+    warning(
       "Incompatible model specification: A missingness model has ",
       "been specified but likelihood aggregation is specified as ",
       "by snapshot. Switching to likelihood aggregation by group.",
       " This has no effect on the nowcast but limits the ",
       "number of threads per chain to the number of groups. To ",
       "silence this warning, set the `likelihood_aggregation` ",
-      "argument in `enw_fit_opts` to 'groups'."
-    ), immediate. = TRUE)
+      "argument in `enw_fit_opts` to 'groups'.",
+      immediate. = TRUE)
+  }
+  return(invisible(NULL))
+}
+
+#' Compare maximum delays specified by user and observed in the data, and 
+#' raise potential warnings.
+#'
+#' @param latest_obs The latest available observations.
+#'
+#' @param max_delay Metadata for the maximum delay produced using 
+#' [enw_metadata_maxdelay()].
+#'
+#' @return NULL
+#'
+#' @family check
+check_max_delay <- function(latest_obs, max_delay) {
+  if (max_delay$obs < max_delay$spec) {
+    warning(
+      "You specified a maximum delay of ",
+      max_delay$spec, " days, ",
+      "but epinowcast will only model delays until the observed maximum delay ",
+      "(", max_delay$obs, " days). ",
+      "Consider adding unobserved delays to your data using ",
+      "`enw_complete_dates` to avoid truncated delay distributions.",
+      immediate. = TRUE
+    )
+  }
+  
+  low_cum <- latest_obs[
+    ,sum(cum_prop_reported<0.8,na.rm=T)/sum(!is.na(cum_prop_reported))
+  ]
+  if (low_cum > 0.5) {
+    warning(
+      "The currently specified maximum reporting delay ",
+      "(", max_delay$spec, " days) ",
+      "covers less than 80% of cases for the majority of reference dates. ",
+      "Consider using a larger maximum delay to avoid model misspecification.",
+      immediate. = TRUE
+    )
   }
   return(invisible(NULL))
 }
