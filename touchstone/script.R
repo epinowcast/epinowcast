7431572a4acf0387f4b33b0357741e0ab5e56d58
Sam <s.e.abbott12@gmail.com>
2023-04-17 17:43:37 +0100

    drop use of path pinning

--- touchstone/script.R
+++ touchstone/script.R
@@ -10,14 +10,14 @@ touchstone::pin_assets("inst/examples")
 # run benchmarks
 touchstone::benchmark_run(
   simple = {
-    source(touchstone::path_pinned_asset("inst/examples/germany_simple.R"))
+    source("inst/examples/germany_simple.R")
   },
   n = 3
 )
 
 touchstone::benchmark_run(
   day_of_week = {
-    source(touchstone::path_pinned_asset("inst/examples/germany_dow.R"))
+    source("inst/examples/germany_dow.R")
   },
   n = 3
 )
@@ -25,16 +25,16 @@ touchstone::benchmark_run(
 
 touchstone::benchmark_run(
   missingness = {
-    source(touchstone::path_pinned_asset("inst/examples/germany_missing.R"))
+    source("inst/examples/germany_missing.R")
   },
   n = 3
 )
 
 touchstone::benchmark_run(
   latent_renewal = {
-    source(touchstone::path_pinned_asset(
+    source(
       "inst/examples/germany_latent_renewal.R"
-    ))
+    )
   },
   n = 3
 )
