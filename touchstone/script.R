d829cda1de2c412766bbd96a0470b687af2cf44f
Sam <s.e.abbott12@gmail.com>
2023-04-19 23:01:23 +0100

    use the right touchstone version

--- touchstone/script.R
+++ touchstone/script.R
@@ -6,32 +6,67 @@ touchstone::branch_install()
 
 # run benchmarks
 touchstone::benchmark_run(
-  simple = {
-    print("Hello world!")
-  },
+  expr_before_benchmark = { source("touchstone/setup.R") },
+  simple = { epinowcast(
+    data = pobs,
+    expectation = enw_expectation(~1, data = pobs),
+    fit = enw_fit_opts(
+      save_warmup = FALSE, pp = TRUE,
+      chains = 2, iter_warmup = 500, iter_sampling = 500,
+    ),
+    obs = enw_obs(family = "poisson", data = pobs)
+  ) },
   n = 3
 )
 
 touchstone::benchmark_run(
-  day_of_week = {
-    source("inst/examples/germany_dow.R")
-  },
+  expr_before_benchmark = { source("touchstone/setup.R") },
+  day_of_week = { epinowcast(
+    data = pobs,
+    report = enw_report(~(1 | day_of_week), data = pobs),
+    fit = enw_fit_opts(
+      save_warmup = FALSE, pp = TRUE,
+      chains = 2, iter_warmup = 500, iter_sampling = 500,
+    ),
+    obs = enw_obs(family = "poisson", data = pobs)
+  ) },
   n = 3
 )
 
 touchstone::benchmark_run(
-  missingness = {
-    source("inst/examples/germany_missing.R")
-  },
+  expr_before_benchmark = { source("touchstone/missing-setup.R") },
+  missingness = { epinowcast(
+    data = pobs,
+    missing = enw_missing(~ (1 | week), data = pobs),
+    report = enw_report(~ (1 | day_of_week), data = pobs),
+    fit = enw_fit_opts(
+      save_warmup = FALSE, pp = TRUE,
+      chains = 2, iter_warmup = 500, iter_sampling = 500,
+    ),
+    obs = enw_obs(family = "poisson", data = pobs)
+  ) },
   n = 3
 )
 
 touchstone::benchmark_run(
-  latent_renewal = {
-    source(
-      "inst/examples/germany_latent_renewal.R"
-    )
-  },
+  expr_before_benchmark = { source("touchstone/setup.R") },
+  latent_renewal = { epinowcast(
+    data = pobs,
+    expectation = enw_expectation(
+      r = ~ 0 + (1 | day),
+      generation_time = c(0.1, 0.4, 0.4, 0.1),
+      observation = ~ (1 | day_of_week),
+      latent_reporting_delay = 0.4 * c(0.05, 0.3, 0.6, 0.05),
+      data = pobs
+    ),
+    reference = enw_reference(~1, data = pobs),
+    report = enw_report(~(1 | day_of_week), data = pobs),
+    fit = enw_fit_opts(
+      save_warmup = FALSE, pp = TRUE,
+      chains = 2, iter_warmup = 500, iter_sampling = 500,
+    ),
+    obs = enw_obs(family = "poisson", data = pobs)
+  ) },
   n = 3
 )
 
