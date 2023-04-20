d6c27cbf4ada2fa2c41d7806503fcd558859c3c9
Sam <s.e.abbott12@gmail.com>
2023-04-20 12:30:00 +0100

    update touchstone script

--- touchstone/script.R
+++ touchstone/script.R
@@ -6,27 +6,48 @@ touchstone::branch_install()
 
 # run benchmarks
 touchstone::benchmark_run(
-  expr_before_benchmark = { source("setup.R") },
-  simple = { epinowcast(
+  expr_before_benchmark = { source("touchstone/setup.R") },
+  preprocessing = { source("touchstone/setup.R") },
+  n = 5
+)
+
+touchstone::benchmark_run(
+  expr_before_benchmark = { source("touchstone/setup.R") },
+  simple_model = { epinowcast(
+    data = pobs,
+    expectation = enw_expectation(~1, data = pobs),
+    fit = enw_fit_opts(
+      save_warmup = FALSE, pp = FALSE,
+      chains = 2, iter_warmup = 500, iter_sampling = 500,
+    ),
+    obs = enw_obs(family = "poisson", data = pobs)
+  ) },
+  n = 5
+)
+
+touchstone::benchmark_run(
+  expr_before_benchmark = { source("touchstone/setup.R") },
+  simple_negbin_model_with_pp = { epinowcast(
     data = pobs,
     expectation = enw_expectation(~1, data = pobs),
     fit = enw_fit_opts(
       save_warmup = FALSE, pp = TRUE,
       chains = 2, iter_warmup = 500, iter_sampling = 500,
     ),
-    obs = enw_obs(family = "poisson", data = pobs)
+    obs = enw_obs(family = "negbin", data = pobs)
   ) },
-  n = 3
+  n = 5
 )
+
 
 touchstone::benchmark_run(
   expr_before_benchmark = { source("touchstone/setup.R") },
-  day_of_week = { epinowcast(
+  day_of_week_model = { epinowcast(
     data = pobs,
     report = enw_report(~(1 | day_of_week), data = pobs),
     fit = enw_fit_opts(
-      save_warmup = FALSE, pp = TRUE,
-      chains = 2, iter_warmup = 500, iter_sampling = 500,
+      save_warmup = FALSE, pp = FALSE,
+      chains = 2, iter_warmup = 500, iter_sampling = 250,
     ),
     obs = enw_obs(family = "poisson", data = pobs)
   ) },
@@ -35,13 +56,13 @@ touchstone::benchmark_run(
 
 touchstone::benchmark_run(
   expr_before_benchmark = { source("touchstone/missing-setup.R") },
-  missingness = { epinowcast(
+  missingness_model = { epinowcast(
     data = pobs,
     missing = enw_missing(~ (1 | week), data = pobs),
     report = enw_report(~ (1 | day_of_week), data = pobs),
     fit = enw_fit_opts(
-      save_warmup = FALSE, pp = TRUE,
-      chains = 2, iter_warmup = 500, iter_sampling = 500,
+      save_warmup = FALSE, pp = FALSE,
+      chains = 2, iter_warmup = 500, iter_sampling = 250,
     ),
     obs = enw_obs(family = "poisson", data = pobs)
   ) },
@@ -50,10 +71,10 @@ touchstone::benchmark_run(
 
 touchstone::benchmark_run(
   expr_before_benchmark = { source("touchstone/setup.R") },
-  latent_renewal = { epinowcast(
+  latent_renewal_model = { epinowcast(
     data = pobs,
     expectation = enw_expectation(
-      r = ~ 0 + (1 | day),
+      r = ~ 1 + rw(week),
       generation_time = c(0.1, 0.4, 0.4, 0.1),
       observation = ~ (1 | day_of_week),
       latent_reporting_delay = 0.4 * c(0.05, 0.3, 0.6, 0.05),
@@ -62,8 +83,8 @@ touchstone::benchmark_run(
     reference = enw_reference(~1, data = pobs),
     report = enw_report(~(1 | day_of_week), data = pobs),
     fit = enw_fit_opts(
-      save_warmup = FALSE, pp = TRUE,
-      chains = 2, iter_warmup = 500, iter_sampling = 500,
+      save_warmup = FALSE, pp = FALSE,
+      chains = 2, iter_warmup = 500, iter_sampling = 250,
     ),
     obs = enw_obs(family = "poisson", data = pobs)
   ) },
