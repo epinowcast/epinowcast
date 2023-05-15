65b0033660a3bab45affba38359554323af56700
Sam <s.e.abbott12@gmail.com>
2023-05-15 23:08:35 +0100

    add a touchstone benchmark for the multi-group model

--- touchstone/setup.R
+++
@@ -1,5 +0,0 @@
-# Run preprocessing script
-source("touchstone/preprocessing.R")
-
-# Compile the model for use outside of the benchmark
-model <- enw_model(threads = FALSE, target_dir = "touchstone")
