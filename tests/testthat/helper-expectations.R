49d168bc2a04dcdfac37d60b82045e2634fb8fe1
Sam <s.e.abbott12@gmail.com>
2023-09-11 15:51:39 +0100

    add integration tests for non parametric models in epinowcast()

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -31,3 +31,12 @@ expect_zero_length_or_not <- function(vars, vars_list) {
     vars_list[!names(vars_list) %in% vars], \(x)(expect_true(length(x) > 0))
   )
 }
+
+expect_convergence <- function(
+  nowcast, per_dts = 0.05, treedepth = 10, rhat = 1.05
+) {
+  expect_identical(class(nowcast$fit[[1]])[1], "CmdStanMCMC")
+  expect_lt(nowcast$per_divergent_transitions, per_dts)
+  expect_lt(nowcast$max_treedepth, treedepth)
+  expect_lt(nowcast$max_rhat, rhat)
+}
--- tests/testthat/test-epinowcast.R
+++ tests/testthat/test-epinowcast.R
@@ -82,12 +82,9 @@ test_that("epinowcast can fit a simple reporting model", {
       "no_at_max_treedepth", "per_at_max_treedepth", "run_time"
     )
   )
-  expect_identical(class(nowcast$fit[[1]])[1], "CmdStanMCMC")
+  expect_convergence(nowcast)
   expect_type(nowcast$fit_args[[1]], "list")
   expect_type(nowcast$data[[1]], "list")
-  expect_lt(nowcast$per_divergent_transitions, 0.05)
-  expect_lt(nowcast$max_treedepth, 10)
-  expect_lt(nowcast$max_rhat, 1.05)
   expect_error(
     nowcast$fit[[1]]$summary(
       c("refp_mean_int", "refp_sd_int")
@@ -113,12 +110,10 @@ test_that("epinowcast can fit a reporting model with a day of the week random
     ),
     model = model
   ))
-  expect_identical(class(nowcast$fit[[1]])[1], "CmdStanMCMC")
+  expect_convergence(nowcast)
   expect_type(nowcast$fit_args[[1]], "list")
   expect_type(nowcast$data[[1]], "list")
-  expect_lt(nowcast$per_divergent_transitions, 0.05)
-  expect_lt(nowcast$max_treedepth, 10)
-  expect_lt(nowcast$max_rhat, 1.05)
+
   posterior <- as.data.table(nowcast$fit[[1]]$summary())
   regression_posterior <- as.data.table(regression_nowcast$fit[[1]]$summary())
   expect_identical(
@@ -212,12 +207,10 @@ test_that("epinowcast can fit a simple missing data model", {
     fit = fit, model = model
   ))
   # Check convergence
-  expect_identical(class(nowcast$fit[[1]])[1], "CmdStanMCMC")
+  expect_convergence(nowcast)
   expect_type(nowcast$fit_args[[1]], "list")
   expect_type(nowcast$data[[1]], "list")
-  expect_lt(nowcast$per_divergent_transitions, 0.05)
-  expect_lt(nowcast$max_treedepth, 10)
-  expect_lt(nowcast$max_rhat, 1.05)
+
   # Extract posteriors
   posterior <- as.data.table(nowcast$fit[[1]]$summary())
   no_missing_posterior <- as.data.table(no_missing_nowcast$fit[[1]]$summary())
@@ -304,19 +297,84 @@ test_that("epinowcast can fit multiple time series at once", {
       )
     )
   )
-
-  expect_identical(class(nowcast$fit[[1]])[1], "CmdStanMCMC")
+  expect_convergence(nowcast)
   expect_type(nowcast$fit_args[[1]], "list")
   expect_type(nowcast$data[[1]], "list")
-  expect_lt(nowcast$per_divergent_transitions, 0.05)
-  expect_lt(nowcast$max_treedepth, 10)
-  expect_lt(nowcast$max_rhat, 1.05)
 })
-# TODO: Add a test for a simple non-parametric reference date model
+
 test_that("epinowcast can fit a simple non-parametric reference date model", {
+  skip_on_cran()
+  skip_on_local()
 
+  nowcast <- suppressMessages(epinowcast(pobs,
+    reference = enw_reference(
+      parametric = ~ 0, non_parametric = ~  1 + (1 | delay),
+      data = pobs
+    ),
+    fit = enw_fit_opts(
+      sampler = silent_enw_sample,
+      save_warmup = FALSE, pp = TRUE,
+      chains = 2, iter_warmup = 250, iter_sampling = 1000,
+      refresh = 0, show_messages = FALSE
+    ),
+    model = model
+  ))
+  expect_convergence(nowcast)
+  expect_equal(
+    nrow(summary(nowcast, type = "fit", variables = "refnp_beta")), 20
+  )
+  expect_equal(
+    summary(nowcast, type = "fit", variables = "refnp_int")$mean, -1.66,
+    tolerance = 0.1
+  )
+  expect_error(
+    summary(
+      nowcast, type = "fit",
+      variables = c(
+        "refp_mean_int", "refp_sd_int", "refp_mean_beta", "refp_sd_beta"
+      )
+    ),
+    "refp_mean_int, refp_sd_int, refp_mean_beta, refp_sd_beta"
+  )
 })
-# TODO: Add a test for a simple combined parametric and non-parametric reference date model
-test_that("epinowcast can fit a simple combined parametric and non-parametric reference date model", {
 
+test_that("epinowcast can fit a simple combined parametric and non-parametric reference date model", {
+  skip_on_cran()
+  skip_on_local()
+
+  nowcast <- suppressMessages(epinowcast(pobs,
+    reference = enw_reference(
+      parametric = ~ 1, non_parametric = ~ 0 + (1 | delay_cat),
+      data = pobs
+    ),
+    fit = enw_fit_opts(
+      sampler = silent_enw_sample,
+      save_warmup = FALSE, pp = TRUE,
+      chains = 2, iter_warmup = 250, iter_sampling = 1000,
+      refresh = 0, show_messages = FALSE
+    ),
+    model = model
+  ))
+  expect_convergence(nowcast)
+  expect_equal(
+    summary(
+      nowcast, type = "fit", variables = c("refnp_beta_sd", "refnp_beta")
+    )$mean,
+    c(0.27, -0.47, 0.57, 0.56, -0.64),
+    tolerance = 0.1
+  )
+  expect_equal(
+    summary(nowcast, type = "fit", variables = c("refp_mean", "refp_sd")),
+    c(1.5, 3.29),
+    tolerance = 0.1
+  )
+  expect_error(
+    summary(
+      nowcast, type = "fit",
+      variables = c(
+        "refp_mean_beta", "refnp_int"
+      )
+    ),
+    "refp_mean_beta, refnp_int"
+  )
 })
