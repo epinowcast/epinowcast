24568f66c46001c99e64f5048194163485573255
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-11-21 14:26:33 +0000

    address gnumake extensions, no dependency on dev cmdstanr

--- vignettes/knit.R
+++
@@ -1,7 +0,0 @@
-# precompiler for vignettes with long run times
-
-library(knitr)
-
-.args <- commandArgs(trailingOnly = TRUE)
-
-knit(.args[1], .args[2])
--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,13 +1,32 @@
 # Precompiled vignettes with long run times
 library(knitr)
-setwd("vignettes")
 
-markerpat <- "\\.orig$"
+.args <- commandArgs(trailingOnly = TRUE)
 
-tocompile <- list.files(pattern = markerpat)
+if (length(.args) > 0) {
+  warning(
+    "`make`ing %.Rmd from %.Rmd.orig will not work",
+    " without developmental `cmdstanr` installation."
+  )
+  knitr::knit(.args[1], .args[2])
+} else if (requireNamespace("usethis")) {
+  wd <- getwd()
+  # assuming somewhere in the project ...
+  setwd(usethis::proj_path("vignettes"))
+  
+  markerpat <- "\\.orig$"
+  
+  tocompile <- list.files(pattern = markerpat)
+  
+  lapply(tocompile, \(x) {
+    knitr::knit(x, sub(markerpat, "", x))
+  })
+  
+  setwd(wd)  
+} else {
+  stop(
+    "Precompilation requires specific source and destination files,",
+    " or `usethis` package."
+  )
+}
 
-lapply(tocompile, \(x) {
-  knit(x, sub(markerpat, "", x))
-})
-
-setwd("..")
