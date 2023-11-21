e6326ac69e2720d31d982b9cd2d851b334366124
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-11-21 15:01:39 +0000

    purge make-based approach for time being

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,27 +1,12 @@
 # Precompiled vignettes with long run times
 library(knitr)
+library(usethis)
 
-.args <- commandArgs(trailingOnly = TRUE)
-
-if (length(.args) > 0) {
-  warning(
-    "`make`ing %.Rmd from %.Rmd.orig will not work",
-    " without developmental `cmdstanr` installation."
-  )
-  knit(.args[1], .args[2])
-} else if (requireNamespace("usethis")) {
-  wd <- getwd()
-  # assuming somewhere in the project ...
-  setwd(usethis::proj_path("vignettes"))
-  markerpat <- "\\.orig$"
-  tocompile <- list.files(pattern = markerpat)
-  lapply(tocompile, \(x) {
-    knit(x, sub(markerpat, "", x))
-  })
-  setwd(wd)
-} else {
-  stop(
-    "Precompilation requires specific source and destination files,",
-    " or `usethis` package."
-  )
-}
+wd <- getwd() # assuming somewhere in the project ...
+setwd(proj_path("vignettes"))
+markerpat <- "\\.orig$"
+tocompile <- list.files(pattern = markerpat)
+lapply(tocompile, \(x) {
+  knit(x, sub(markerpat, "", x))
+})
+setwd(wd)
