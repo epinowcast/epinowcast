5def3a443d480f5041324ec3c5d1a6ec10339a2f
Michael DeWitt <me.dewitt.jr@gmail.com>
2023-12-15 04:56:49 -0500

    use withr to keep global state consistent

---
+++ tests/testthat/test-enw_get_cache.R
@@ -0,0 +1,30 @@
+test_that("enw_get_cache can retrieve the enw_cache_location as expected", {
+    withr::with_envvar(new = c(enw_cache_location = "models"), 
+    expect_equal(
+        basename(enw_get_cache()), 
+        "models"
+    )
+    )
+
+    expect_equal(
+        enw_get_cache(), 
+        tempdir()
+    )
+})
+
+test_that("enw_set_cache can set the enw_cache_location as expected", {
+    expect_error(enw_set_cache())
+
+    withr::with_envvar(c(enw_cache_location = ""),{
+        expect_equal(enw_set_cache("test"), enw_get_cache())
+    })
+    
+})
+
+test_that("enw_unset_cache can unset the cache directory", {
+    status <- withr::with_envvar(new = c(enw_cache_location = "models"), {
+      enw_unset_cache()
+      Sys.getenv("enw_cache_location")
+    })
+    expect_equal(status,  "")
+})
