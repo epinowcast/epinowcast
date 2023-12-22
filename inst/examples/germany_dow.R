26d044b6c5fddd0214923ec1767fb9f8b8a2bf2e
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-12-22 19:43:39 +0100

    Format germany_dow

--- inst/examples/germany_dow.R
+++ inst/examples/germany_dow.R
@@ -5,31 +5,26 @@ library(data.table)
 # Load and filter germany hospitalisations
 nat_germany_hosp <- germany_covid19_hosp[location == "DE"][age_group %in% "00+"]
 nat_germany_hosp <- enw_filter_report_dates(
-  nat_germany_hosp,
-  latest_date = "2021-10-01"
+  nat_germany_hosp, latest_date = "2021-10-01"
 )
 
 # Make sure observations are complete
 nat_germany_hosp <- enw_complete_dates(
-  nat_germany_hosp,
-  by = c("location", "age_group")
+  nat_germany_hosp, by = c("location", "age_group")
 )
 
 # Make a retrospective dataset
 retro_nat_germany <- enw_filter_report_dates(
-  nat_germany_hosp,
-  remove_days = 40
+  nat_germany_hosp, remove_days = 40
 )
 retro_nat_germany <- enw_filter_reference_dates(
-  retro_nat_germany,
-  include_days = 40
+  retro_nat_germany, include_days = 40
 )
 
 # Get latest observations for the same time period
 latest_obs <- enw_latest_data(nat_germany_hosp)
 latest_obs <- enw_filter_reference_dates(
-  latest_obs,
-  remove_days = 40, include_days = 20
+  latest_obs, remove_days = 40, include_days = 20
 )
 
 # Preprocess observations
