ac4ba46558aae345114169157640c0574debbea4
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-01-05 14:50:15 -0500

    follow our style guide

--- R/zzz.R
+++ R/zzz.R
@@ -5,9 +5,11 @@
 
 enw_startup_message <- function(){
     cache_location <- Sys.getenv("enw_cache_location")
-    if (check_environment_setting) {
-        cli_inform("`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times")
+    if (epinowcast:::check_environment_setting(cache_location)) {
+        msg <- cli_inform("`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times")
     } else {
-        cli_inform("Using `{cache_location}` for the epinowcast model cache location.")
+        msg <- cli_inform("Using `{cache_location}` for the epinowcast model cache location.")
     }
+
+    return(msg)
 }
