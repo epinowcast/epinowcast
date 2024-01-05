53448998fcad26cb357b8a7862aa7504c1e0c4b9
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-01-05 14:40:35 -0500

    make start up testable

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,13 @@
-.onLoad <- function(libname, pkgname) {
+.onLoad <- function(...) {
+    packageStartupMessage(enw_startup_message())
+    return(invisible())
+}
+
+enw_startup_message <- function(){
     cache_location <- Sys.getenv("enw_cache_location")
     if (check_environment_setting) {
-        cli::cli_inform("`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times")
+        cli_inform("`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times")
     } else {
         cli_inform("Using `{cache_location}` for the epinowcast model cache location.")
     }
-
-    return(invisible())
 }
