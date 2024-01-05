b973523b27faacf2b498b1a1985743479cdb83be
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-01-05 16:24:51 -0500

    Make onLoad message testtable and pass

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,20 @@
-.onLoad <- function(...) {
-    packageStartupMessage(enw_startup_message())
-    return(invisible())
+# Using this approach from the cli package on startup messages.
+
+## nocov start
+
+.onLoad <- function(libname, pkgname) {
+    packageStartupMessage(cli_inform(enw_startup_message()))
 }
 
 enw_startup_message <- function(){
     cache_location <- Sys.getenv("enw_cache_location")
-    if (epinowcast:::check_environment_setting(cache_location)) {
-        msg <- cli_inform("`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times")
+    if (check_environment_setting(cache_location)) {
+        msg <- "`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times"
     } else {
-        msg <- cli_inform("Using `{cache_location}` for the epinowcast model cache location.")
+        msg <- sprintf("Using `%s` for the epinowcast model cache location.", cache_location)
     }
 
     return(msg)
 }
+
+## nocov end
--- tests/testthat/test-onLoad.R
+++ tests/testthat/test-onLoad.R
@@ -1,15 +1,15 @@
-test_that(".onLoad alerts user of enw_cache_location", {
-    expect_equal(capture_output_lines(enw_startup_message()), 
-    cli_inform("`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times")
+test_that("onLoad alerts user of enw_cache_location", {
+    expect_equal(enw_startup_message(), 
+    "`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times"
     )
-}
+})
 
 test_that(".onLoad alerts user of enw_cache_location", {
 
     with_envvar(new = c(enw_cache_location = "models"), 
       expect_equal(
-        capture_output_lines(enw_startup_message()), 
-        cli_inform("Using `models` for the epinowcast model cache location.")
+        enw_startup_message(), 
+        "Using `models` for the epinowcast model cache location."
       )
     )
-}
+})
