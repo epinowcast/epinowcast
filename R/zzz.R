9ccffaf61b9cee3f07f1ff80c3ff347af29d498a
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-01-08 11:22:17 -0500

    Message typo

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,7 @@ enw_startup_message <- function(){
     cache_location <- Sys.getenv("enw_cache_location")
     if (check_environment_setting(cache_location)) {
         msg <- c(
-            "!" = "`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times.",
+            "!" = "`enw_cache_location` is not set. Set it using `enw_set_cache` to reduce future Stan compilation times.",
             "i" = "For example: `enw_set_cache(tools::R_user_dir(package = \"epinowcast\", \"cache\"))`.",
             "i" = "See `?enw_set_cache` for details."
         )
