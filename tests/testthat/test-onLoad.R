3795722b38df7126e0c375ba1a88d7aa1f736b56
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-01-08 11:01:53 -0500

    Update test for onLoad

--- tests/testthat/test-onLoad.R
+++ tests/testthat/test-onLoad.R
@@ -1,13 +1,13 @@
 test_that("onLoad alerts user of enw_cache_location", {
-    expect_equal(enw_startup_message(),
-      "`enw_cache_location` is not set it. Set it using `enw_set_cache` to reduce future Stan compilation times"
+    expect_identical(length(enw_startup_message()),
+      3L
     )
 })
 
 test_that(".onLoad alerts user of enw_cache_location", {
 
     with_envvar(new = c(enw_cache_location = "models"),
-      expect_equal(
+      expect_identical(
         enw_startup_message(),
         "Using `models` for the epinowcast model cache location."
       )
