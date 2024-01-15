44eca056c60e55b8bec4c80f84e872604fd66881
Sam <s.e.abbott12@gmail.com>
2024-01-15 16:24:17 +0000

    streamline no linting

--- R/zzz.R
+++ R/zzz.R
@@ -1,22 +1,18 @@
-
-## nocov start
-# nolint start
-
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(cli_inform(enw_startup_message()))
 }
-# nolint end
 
 enw_startup_message <- function() {
     cache_location <- Sys.getenv("enw_cache_location")
     if (check_environment_setting(cache_location)) {
+        # nolint start
         msg <- c(
             "!" = "`enw_cache_location` is not set. Set it using `enw_set_cache`
-            to reduce future Stan compilation times.", # nolint
-            "i" = "For example: `enw_set_cache(tools::R_user_dir(package = 
-            \"epinowcast\", \"cache\"))`.", # nolint
-            "i" = "See `?enw_set_cache` for details." # nolint
-        ) #nolint
+            to reduce future Stan compilation times.",
+            "i" = "For example: `enw_set_cache(tools::R_user_dir(package =
+            \"epinowcast\", \"cache\"))`.",
+            "i" = "See `?enw_set_cache` for details."
+    # nolint end 
     } else {
         msg <- sprintf("Using `%s` for the epinowcast model cache location.",
           cache_location)
@@ -24,7 +20,3 @@ enw_startup_message <- function() {
 
     return(msg)
 }
-
-
-
-## nocov end
