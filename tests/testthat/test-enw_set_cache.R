b7af303a53d556291c0ba94f436ba6406fe7f528
Sam <s.e.abbott12@gmail.com>
2024-01-17 23:12:36 +0000

    expand and check set_cache tests

--- tests/testthat/test-enw_set_cache.R
+++ tests/testthat/test-enw_set_cache.R
@@ -1,3 +1,62 @@
+test_that("enw_set_cache() can set the session cache directory", {
+  current_cache <- suppressMessages(enw_get_cache())
+  suppressMessages(withr::with_envvar(c(enw_cache_location = ""), {
+    path <- file.path(tempdir(), "test_session_cache")
+    enw_set_cache(path, type = "session")
+    status <- Sys.getenv("enw_cache_location")
+    expect_identical(status, suppressWarnings(normalizePath(path)))
+  }))
+  suppressMessages(enw_set_cache(current_cache, type = "session"))
+})
+
+test_that("enw_set_cache() can set the persistent cache directory", {
+  suppressMessages(withr::with_dir(
+    tempdir(), {
+      withr::with_envvar(c(enw_cache_location = "testy"), {
+        file.create(".Renviron")
+        path <- file.path(tempdir(), "test_persistent_cache")
+        enw_set_cache(path, type = "persistent")
+        env_contents <- readLines(".Renviron")
+        expect_true(
+            any(grepl(
+                paste0("enw_cache_location=\"",
+                suppressWarnings(normalizePath(path)), "\""),
+                env_contents, fixed = TRUE
+            ))
+        )
+        status <- Sys.getenv("enw_cache_location")
+        expect_identical(status, "testy")
+      })
+  }))
+})
+
+test_that("enw_set_cache() can set both the session and persistent cache directories", { # nolint
+  current_cache <- suppressMessages(enw_get_cache())
+  suppressMessages(withr::with_dir(
+    tempdir(), {
+      file.create(".Renviron")
+      path <- file.path(tempdir(), "test_persistent_cache")
+      enw_set_cache(path, type = "all")
+      env_contents <- readLines(".Renviron")
+      expect_true(
+        any(grepl(
+            paste0("enw_cache_location=\"",
+            suppressWarnings(normalizePath(path)), "\""),
+            env_contents, fixed = TRUE
+        ))
+      )
+    status <- Sys.getenv("enw_cache_location")
+    expect_identical(status, suppressWarnings(normalizePath(path)))
+    }
+  ))
+  suppressMessages(enw_set_cache(current_cache, type = "session"))
+})
+
+test_that("enw_set_cache() fails as expected with incorrect input", {
+  expect_error(enw_set_cache(NULL))
+  expect_error(enw_set_cache(1))
+})
+
 persistent_tempdir <- file.path(tempdir(), "enw_set_cache")
 test_model_cache <- function() {
     withr::with_envvar(
@@ -10,27 +69,22 @@ test_model_cache <- function() {
     )
 }
 
-test_that("enw_model can access enw_cache_location", {
-    skip_on_cran()
-    run_initial <- test_model_cache()
-    run_secondary <- test_model_cache()
-    expect_lt(run_secondary, run_initial)
+test_that("enw_model() can access enw_cache_location", {
+  skip_on_cran()
+  run_initial <- test_model_cache()
+  run_secondary <- test_model_cache()
+  expect_lt(run_secondary, run_initial)
 })
 
 cli::test_that_cli("alert", {
     skip_on_cran()
     local_edition(3)
-    current_cache <- enw_get_cache()
+    current_cache <- suppressMessages(enw_get_cache())
     testthat::expect_snapshot({
     withr::with_envvar(
         new = c(enw_cache_location = "initial_location"), {
-            enw_set_cache("second_location")
+            enw_set_cache("second_location", type = "session")
         })
     })
-    enw_set_cache(current_cache)
-})
-
-test_that("enw_set_cache() fails as expected with incorrect input", {
-    expect_error(enw_set_cache())
-    expect_error(enw_set_cache(1))
+    suppressMessages(enw_set_cache(current_cache, type = "session"))
 })
