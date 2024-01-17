de51bc0ad681f71b310a18e469d896f5e531524e
Sam <s.e.abbott12@gmail.com>
2024-01-17 23:26:00 +0000

    lint

--- tests/testthat/test-enw_unset_cache.R
+++ tests/testthat/test-enw_unset_cache.R
@@ -30,12 +30,14 @@ test_that("enw_unset_cache() can unset the persistent cache directory", {
     enw_set_cache(".", type = "persistent")
     enw_unset_cache(type = "persistent")
     expect_false(
-      any(grepl("enw_cache_location", readLines(".Renviron")))
+      any(grepl("enw_cache_location", readLines(".Renviron"), fixed = TRUE))
     )
   }))
 })
 
 test_that("enw_unset_cache can unset both the session and persistent cache directories", { # nolint
+  env_location <- Sys.getenv("HOME")
+  env_path <- file.path(env_location, ".Renviron")
   withr::with_envvar(c(enw_cache_location = "models"), {
     suppressMessages(withr::with_dir(
       tempdir(), {
@@ -43,13 +45,13 @@ test_that("enw_unset_cache can unset both the session and persistent cache direc
       enw_set_cache(".", type = "persistent")
       enw_unset_cache(type = "all")
       expect_false(
-        any(grepl("enw_cache_location", readLines(".Renviron")))
+        any(grepl("enw_cache_location", readLines(".Renviron"), fixed = TRUE))
       )
       expect_identical(Sys.getenv("enw_cache_location"), "")
     }))
   })
-  if (file.exists("~/.Renviron")) {
-    readRenviron("~/.Renviron")
+  if (file.exists(env_path)) {
+    readRenviron(env_path)
   }
 })
 
