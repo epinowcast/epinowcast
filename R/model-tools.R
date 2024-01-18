d4121be77db80b2713810621b3e2da5c3f781802
Sam <s.e.abbott12@gmail.com>
2024-01-18 10:41:25 +0000

    use path not candidate path so that snapshots are stable on windows

--- R/model-tools.R
+++ R/model-tools.R
@@ -537,7 +537,7 @@ enw_set_cache <- function(path, type = c("session", "persistent", "all")) {
 
   if (type == "session" || type == "all") {
     cli::cli_alert_success(
-      "Set `enw_cache_location` to {candidate_path}"
+      "Set `enw_cache_location` to {path}"
     )
     Sys.setenv(enw_cache_location = candidate_path)
   }
