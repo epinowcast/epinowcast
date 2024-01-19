80f7473a56265792f6a449bc55ac8e59ceb542be
Sam Abbott <s.e.abbott12@gmail.com>
2024-01-19 17:35:04 +0000

    Update R/model-tools.R
    Co-authored-by: Sebastian Funk <sebastian.funk@lshtm.ac.uk>

--- R/model-tools.R
+++ R/model-tools.R
@@ -578,10 +578,10 @@ enw_unset_cache <- function(type = c("session", "persistent", "all")) {
   if (type == "session" || type == "all") {
     prior_location <- Sys.getenv("enw_cache_location")
     if (prior_location != "") {
+      Sys.unsetenv("enw_cache_location")
       cli::cli_alert_success(
         "Removed `enw_cache_location = {prior_location}` from the local environment." # nolint line_length
       )
-      Sys.unsetenv("enw_cache_location")
         if (type == "session") {
           cli::cli_alert_info(
             "To revert to the persistent cache (if set), run `readRenviron('~/.Renviron')`" # nolint line_length
