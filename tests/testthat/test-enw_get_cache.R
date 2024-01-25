fee54a866db8334176d2acbd0426a2d41a736a02
Sam <s.e.abbott12@gmail.com>
2024-01-25 21:20:50 +0000

    give up on platform stable paths

--- tests/testthat/test-enw_get_cache.R
+++ tests/testthat/test-enw_get_cache.R
@@ -13,3 +13,24 @@ test_that("enw_get_cache() can retrieve the enw_cache_location as expected", {
     )
   ))
 })
+
+enw_model_time <- function() {
+  time_start <- Sys.time()
+  suppressMessages(enw_model(verbose = FALSE))
+  time_out <- Sys.time()
+  difftime(time_out, time_start, units = "secs")[[1]]
+}
+
+test_that("enw_model() can access enw_cache_location", {
+  skip_on_cran()
+  skip_on_local()
+  withr::with_tempdir(
+    withr::with_envvar(
+      new = c(enw_cache_location = "test"), {
+        run_initial <- enw_model_time()
+        run_secondary <- enw_model_time()
+        expect_lt(run_secondary, run_initial)
+      }
+    )
+  )
+})
--- tests/testthat/test-enw_set_cache.R
+++ tests/testthat/test-enw_set_cache.R
@@ -68,29 +68,10 @@ test_that("enw_set_cache() fails as expected with incorrect input", {
   expect_error(enw_set_cache(1))
 })
 
-enw_model_time <- function() {
-  time_start <- Sys.time()
-  suppressMessages(enw_model(verbose = FALSE))
-  time_out <- Sys.time()
-  difftime(time_out, time_start, units = "secs")[[1]]
-}
-
-test_that("enw_model() can access enw_cache_location", {
-  skip_on_cran()
-  withr::with_tempdir(
-    withr::with_envvar(
-      new = c(enw_cache_location = "test"), {
-        run_initial <- enw_model_time()
-        run_secondary <- enw_model_time()
-        expect_lt(run_secondary, run_initial)
-      }
-    )
-  )
-})
-
 cli::test_that_cli("alert", {
   skip_on_cran()
-  skip_on_local()
+  skip_on_os("windows")
+  skip_on_os("mac")
   local_edition(3)
   current_cache <- suppressMessages(enw_get_cache())
   expect_snapshot({
