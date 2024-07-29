31981992ae2fdfd9ce8064578886bf4b8f9cd510
Jessalyn Sebastian <jessalyn.sebastian@uci.edu>
2024-07-29 15:18:59 -0700

    updated test file for expected_obs to account for log probs issue

--- tests/testthat/test-stan_expected_obs.R
+++ tests/testthat/test-stan_expected_obs.R
@@ -10,16 +10,16 @@ enw_stan_to_r(
 # note these tests require enw_stan_to_r() to be run first
 
 test_that("expected_obs() aggregates probabilities correctly", {
-  tar_obs <- 0
-  lh <- c(0.1, 0.2, 0.3, 0.4)
-  ref_as_p <- 1
-  agg_probs <- 1
-  agg_indicator <- matrix(c(0, 0, 0, 0,
-                            1, 1, 0, 0,
-                            0, 0, 0, 0,
-                            0, 0, 1, 1), nrow = 4, byrow = TRUE)
-  exp_obs <- expected_obs(tar_obs, lh, ref_as_p, agg_probs, agg_indicator)
-  expect_equal(exp_obs, c(0.0, 0.3, 0.0, 0.7), tolerance = 1e-15)
-  # Note below is only true since ref_as_p is 1
-  expect_identical(sum(exp_obs), sum(lh))
+  tar_obs <- log(1)
+  lh <- log(
+    (plnorm(1:30, 1.5, 2) - plnorm(0:29, 1.5, 2)) / plnorm(30, 1.5, 2)
+    )
+  agg_probs <- matrix(c(rep(0, times = 30 * 4),
+                        rep(c(rep(1, times = 5),
+                              rep(0, times = 6 * 5 + 30 * 4)),
+                            times = 5),
+                        rep(1, times = 5)), ncol = 30, byrow = TRUE)
+  exp_obs <- expected_obs(tar_obs, lh, ref_as_p = 1, agg_probs = 1, agg_probs)
+  exp_output <- as.vector(tar_obs + log(agg_probs %*% exp(lh)))
+  expect_equal(object = exp_obs, expected = exp_output, tolerance = 1e-15)
 })
