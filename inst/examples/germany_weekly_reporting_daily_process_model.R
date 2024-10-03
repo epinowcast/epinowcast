81dedefd0da532f76c6759a32e08c9cac41953d4
Sam <s.e.abbott12@gmail.com>
2024-10-03 19:30:30 +0100

    first pass at restructuring things

--- inst/examples/germany_weekly_reporting_daily_process_model.R
+++ inst/examples/germany_weekly_reporting_daily_process_model.R
@@ -90,7 +90,7 @@ metadata <- pobs$metareference[[1]] |>
   _[, day_of_week := weekdays(report_date)] |>
   _[, report := ifelse(day_of_week == "Wednesday", 1, 0)]
 
-# Generic munging to make it easier too find reporting
+# Generic munging to make it easier to find reporting
 metadata <- metadata |>
   _[, cum_report := cumsum(report) + 1, by = .(.group, date)] |>
   _[day_of_week == "Wednesday", cum_report := cum_report - 1]
@@ -131,45 +131,18 @@ metadata <- metadata[, {
   mget(names(.SD))
 }, by = "date"]
 
-# You can view the matrix like this:
-# print(agg_matrix)
-
-# If you want to see it in a more readable format:
-# library(knitr)
-# kable(agg_matrix)
-
-# Define the structural argument for probability aggregation
-# We are assigning one 35x35 matrix to each reference day, depending on the DOW
-# Report day is Wednesday, and as coded above everything up to & including Wednesday
-# is reported on Wednesday.
-# Scratch in a function to make the agg_indicator for each wday
-generate_agg_indicator <- function(reference_dow_idx, reporting_dow_idx, max_delay) {
-  # Indices are as from lubridate::wday
-  d <- ifelse(reference_dow_idx < reporting_dow_idx,
-              reporting_dow_idx - reference_dow_idx,
-              7 - (reference_dow_idx - reporting_dow_idx))
-  row_idxs <- d + seq(1, max_delay, by = 7)
-  row_idxs <- col_breaks <- row_idxs[row_idxs <= max_delay]
-  col_idxs <- lapply(col_breaks, function(idx){
-    return(max(idx - 6, 1):idx)
+# Split by date and group, and get delay_ vars as a matrix
+agg_indicators <- metadata |>
+  _[,
+    c(".group", "date", grep("^delay_", names(metadata), value = TRUE)),
+    with = FALSE
+  ] |>
+  split(by = ".group", drop = TRUE) |>
+  purrr::map(\(group_data) {
+    group_data |>
+      split(by = "date", drop = TRUE) |>
+      purrr::map(\(x) t(as.matrix(x[, -c(".group", "date")])))
   })
-  agg_matrix <- matrix(0, nrow = max_delay, ncol = max_delay)
-  # Replace desired elements with 1
-  for (i in seq_along(col_idxs)) {
-    agg_matrix[row_idxs[i], col_idxs[[i]]] <- 1
-  }
-  return(agg_matrix)
-}
-ref_day_matrix_list <- lapply(1:7, FUN = function(day) { 
-  generate_agg_indicator(day, 4, 35)
-})
-# Then get array of these matrices
-ref_t_wdays <- wday(pobs$metareference[[1]]$date)
-agg_indicators <- lapply(seq_along(ref_t), function(t) {
-  ref_day_matrix_list[[ref_t_wdays[t]]]
-})
-agg_indicators <- array(unlist(agg_indicators), dim = c(35, 35, length(ref_t)))
-agg_indicators <- aperm(agg_indicators, c(3, 1, 2))
 
 # Fit a simple nowcasting model with fixed growth rate and a
 # log-normal reporting distribution.
@@ -178,7 +151,7 @@ nowcast <- epinowcast(pobs,
   expectation = enw_expectation(~1, data = pobs),
   report = enw_report(structural = agg_indicators, data = pobs),
   fit = enw_fit_opts(
-    init_method = "sample",
+    init_method = "prior",
     save_warmup = FALSE, pp = TRUE,
     chains = 2, iter_warmup = 500, iter_sampling = 500,
   ),
