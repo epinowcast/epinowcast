9329c4a438c61d2fea76d7adad33d88fc84d5e9e
Sam Abbott <contact@samabbott.co.uk>
2025-10-13 15:28:36 +0100

    Fix: Remove duplicate deprecated enw_filter_delay function
    Removes the incorrectly named deprecated wrapper function that was
    creating a duplicate definition of enw_filter_delay. This function
    was intended to be named enw_delay_filter (to match the deprecation
    message) but was never corrected before being marked for removal.
    
    The actual enw_filter_delay function (the non-deprecated version)
    remains unchanged and functional.

--- R/preprocess.R
+++ R/preprocess.R
@@ -503,31 +503,6 @@ enw_latest_data <- function(obs) {
   return(latest_data[])
 }
 
-#' Filter observations to restrict the maximum reporting delay
-#'
-#' @description `r lifecycle::badge("deprecated")`
-#'
-#' @return A `data.frame` filtered so that dates by report are less than or
-#' equal the reference date plus the maximum delay.
-#'
-#' @inheritParams enw_filter_delay
-#' @inheritParams enw_add_incidence
-#' @inheritParams enw_preprocess_data
-#' @importFrom lifecycle deprecate_stop
-#' @family preprocess
-#' @keywords internal
-#' @export
-enw_filter_delay <- function(obs, max_delay, timestep = "day") {
-  lifecycle::deprecate_warn(
-    when = "0.2.3",
-    what = "enw_delay_filter()",
-    with = "enw_filter_delay()",
-    details = "Please file an issue if deprecating this \
-      function has caused any issues."
-  )
-  return(enw_filter_delay(obs, max_delay, timestep))
-}
-
 #' Filter observations to have a consistent maximum delay period
 #'
 #' @return A `data.frame` filtered so that dates by report are less than or
